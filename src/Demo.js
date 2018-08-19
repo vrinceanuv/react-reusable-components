00670a083fdc234e14e2f881c214d72e58ab6575
vrinceanuv <vrinceanu.valentin@gmail.com>
2018-08-19 16:56:37 +0300

    Initial commit

---
+++ src/Demo.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Demo.css';
+
+class Demo extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React Reusable Components</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Demo;
